9d9ead3ee4940a10f98cf52ea2f22a6e0e1850ca
Jourden Riley <jourdenrai@gmail.com>
2020-09-25 10:15:57 -0500

    repository keeps getting corrupted

--- small_problems/advanced_1/binarySearch.js
+++ small_problems/advanced_1/binarySearch.js
@@ -1,7 +1,24 @@
-function binarySearch(arr,search) {
-  
+function binarySearch(arr,searchItem) {
+  let high = arr.length - 1;
+  let low = 0;
+
+  while (low <= high) {
+    let mid = low + Math.floor((high - low) / 2);
+    if (arr[mid] === searchItem) {
+      return mid;
+    } else if (arr[mid] < searchItem) low = mid + 1;
+    else high = mid + 1;
+  }
+  return -1;
 }
 
 let yellowPages = ['Apple Store', 'Bags Galore', 'Bike Store', 'Donuts R Us', 'Eat a Lot', 'Good Food', 'Pasta Place', 'Pizzeria', 'Tiki Lounge', 'Zooper'];
-console.log(binarySearch(yellowPages, 'Pizzeria'));                   // 7
+binarySearch(yellowPages, 'Pizzeria');                   // 7
 binarySearch(yellowPages, 'Apple Store');                // 0
+
+binarySearch([1, 5, 7, 11, 23, 45, 65, 89, 102], 77);    // -1
+binarySearch([1, 5, 7, 11, 23, 45, 65, 89, 102], 89);    // 7
+binarySearch([1, 5, 7, 11, 23, 45, 65, 89, 102], 5);     // 1
+
+binarySearch(['Alice', 'Bonnie', 'Kim', 'Pete', 'Rachel', 'Sue', 'Tyler'], 'Peter');  // -1
+binarySearch(['Alice', 'Bonnie', 'Kim', 'Pete', 'Rachel', 'Sue', 'Tyler'], 'Tyler');  // 6
